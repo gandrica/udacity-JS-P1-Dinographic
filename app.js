7dce5d5d0a70cf443f3bfb4bf84babee9b11d28f
gandrica <gandrica1983@gmail.com>
2024-01-10 17:11:12 +0100

    first commit

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
+// Create Dino Constructor
+function Dino(data) {
+	this.species = data.species;
+	this.weight = data.weight;
+	this.height = data.height;
+	this.diet = data.diet;
+	this.where = data.where;
+	this.when = data.when;
+	this.fact = data.fact;
+}
 
-    // Create Dino Constructor
+// Create Dino Objects
 
+// Create Human Object
+function Human(data) {
+	this.name = data.name;
+	this.weight = data.weight;
+	this.heightInFeet = data.heightInFeet;
+	this.heightInInches = data.heightInInches;
+	this.diet = data.diet;
+}
+// Use IIFE to get human data from form
 
-    // Create Dino Objects
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Create Human Object
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    // Use IIFE to get human data from form
+// Generate Tiles for each Dino in Array
 
+// Add tiles to DOM
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
-
-    // Remove form from screen
-
+// Remove form from screen
 
 // On button click, prepare and display infographic
